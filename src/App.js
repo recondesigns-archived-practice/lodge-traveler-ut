610c5d7b14a2394ddb03914de346a9c6cd352c55
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-06-20 18:43:09 -0500

    Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps'
+
+function Map() {
+  return (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 30.7582, lng: -98.2284 }}
+    />
+  )
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map))
 
 export default function App() {
   return (
-    <div>
-      <p>hello world</p>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <h1>{'Lodge Traveler - UT'}</h1>
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
+        loadingElement={<div style={{ height: '100%' }} />}
+        containerElement={<div style={{ height: '100%' }} />}
+        mapElement={<div style={{ height: '100%' }} />}
+      />
     </div>
   )
 }
